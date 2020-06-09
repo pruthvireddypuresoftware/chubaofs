cac5037709c2ba446ba5094d6c3798a53f49dc1e
Mofei Zhang <mofei2816@gmail.com>
2020-06-09 17:03:08 +0800

    Enhancement: improve metadata persist
    1. Reduce the number of heap memory applications by reusing buffers when
    persisting metadata.
    2. Before persisting, compare the current ApplyID with the last successfully
    persisted ApplyID, and then trigger the persistence when the gap exceeds 1000.
    
    Signed-off-by: Mofei Zhang <mofei2816@gmail.com>

--- metanode/partition.go
+++ metanode/partition.go
@@ -215,6 +215,8 @@ type metaPartition struct {
 	extReset      chan struct{}
 	vol           *Vol
 	manager       *metadataManager
+
+	persistedApplyID uint64
 }
 
 // Start starts a meta partition.
@@ -522,7 +524,10 @@ func (mp *metaPartition) store(sm *storeMsg) (err error) {
 		_ = os.Rename(backupDir, snapshotDir)
 		return
 	}
-	err = os.RemoveAll(backupDir)
+	if err = os.RemoveAll(backupDir); err != nil {
+		return
+	}
+	mp.updatePersistedApplyID(sm.applyIndex)
 	return
 }
 
